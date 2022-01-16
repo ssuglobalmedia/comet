ebe2722e68511e154f9705069256bbd4d67a808f
EATSTEAK <eatsteak95@gmail.com>
2022-01-17 05:37:36 +0900

    chore: add ModuleDefinition type

--- packages/client/src/global.d.ts
+++ packages/client/src/global.d.ts
@@ -1,5 +1,17 @@
-/// <reference types="@sveltejs/kit" />
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { SvelteComponent } from 'svelte';
 
+/// <reference types="@sveltejs/kit" />
 interface ImportMetaEnv {
 	VITE_BASE_URL: string;
 }
+
+/* Module */
+export type ModuleDefinition = {
+	module: string;
+	title: string;
+	pictogram: typeof SvelteComponent;
+	icon: typeof SvelteComponent;
+	accessibleGroup: string;
+	description: string;
+};
