19f13b0746cdf5478f57f3d453ef48c4ea46bd69
EATSTEAK <eatsteak95@gmail.com>
2022-01-17 23:49:47 +0900

    chore: move top-level module route to directory

--- packages/client/src/routes/module/dashboard/index.svelte
+++ packages/client/src/routes/module/dashboard/index.svelte
@@ -1,6 +1,6 @@
 <script>
-    import ModuleDashboard from "../../components/module/dashboard/ModuleDashboard.svelte";
-    import {modules} from "$lib/modules";
+    import ModuleDashboard from "../../../components/module/dashboard/ModuleDashboard.svelte";
+    import {modules} from "$lib/modules.js";
     const filteredModules = {...modules};
     delete filteredModules.dashboard;
 </script>
