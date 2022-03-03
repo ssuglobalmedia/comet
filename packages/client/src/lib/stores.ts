a654e70894727d603281f3eabe783e7f03b453a9
EATSTEAK <eatsteak95@gmail.com>
2022-03-04 02:05:58 +0900

    chore: extract inline type

--- packages/client/src/lib/stores.ts
+++ packages/client/src/lib/stores.ts
@@ -2,7 +2,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { readable } from 'svelte/store';
 import type { Readable } from 'svelte/store';
-import type { UserInfo } from 'types';
+import type { CometResponse, UserInfo } from 'types';
 import { fetchWithAuth } from '$lib/module/auth';
 import { variables } from '$lib/variables';
 import { browser } from '$app/env';
@@ -12,11 +12,11 @@ export const userInfo: Readable<UserInfo> = readable<UserInfo>(undefined, (set) 
 		set(undefined);
 		fetchWithAuth(`${variables.baseUrl as string}/api/module/auth/user/get`)
 			.then((res) => res.json())
-			.then((res: { success: boolean, result?: any, error?: number, error_description?: string }) => {
-				if(res.success) {
+			.then((res: CometResponse) => {
+				if (res.success) {
 					return res.result as UserInfo;
 				}
-				throw new Error(`Request error ${res.error}: ${res.error_description}`)
+				throw new Error(`Request error ${res.error}: ${res.error_description}`);
 			})
 			.then((info: UserInfo) => set(info))
 			.catch((err) => {
