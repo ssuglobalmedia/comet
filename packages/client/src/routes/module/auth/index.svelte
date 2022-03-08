0cd3b2188194d71396f917b7398ff0d208713693
EATSTEAK <eatsteak95@gmail.com>
2022-03-09 05:03:05 +0900

    feat: apply actual user data

--- packages/client/src/routes/module/auth/index.svelte
+++ packages/client/src/routes/module/auth/index.svelte
@@ -1,18 +1,26 @@
 <script lang="ts">
-  import { Button, Column, DataTable, Grid } from "carbon-components-svelte";
+  import { Button, Checkbox, Column, DataTable, Grid } from "carbon-components-svelte";
   import { browser } from "$app/env";
   import { variables } from "$lib/variables";
-  import { fetchWithAuth } from "$lib/module/auth";
+  import { fetchWithAuth, groupDisplayName } from "$lib/module/auth";
   import type {CometResponse, User} from "mirinae-comet";
 
-  let users: Array<User> = [];
+  type UserCell = {
+    id: string;
+  } & User;
+
+  let users: Array<UserCell> = [];
+
+  const transformUser = (user: User): UserCell => ({
+    id: user.userId,
+    ...user,
+    userGroup: groupDisplayName[user.userGroup] ?? '알 수 없음'
+  })
 
   if(browser) {
     fetchWithAuth(`${variables.baseUrl as string}/api/module/auth/user/query`).then((res) => res.json()).then((res: CometResponse) => {
-      if(res.success) {
-        users = res.result as Array<User>;
-      }
-      throw new Error(`Request error ${res.error}: ${res.error_description}`)
+      if(!res.success) throw new Error(`Request error ${res.error}: ${res.error_description}`);
+      users = (res.result as Array<User>).map(transformUser);
     }).catch((err) => {
       console.error(err);
     });
@@ -33,27 +41,19 @@
   headers={[
   { key: "userName", value: "사용자 이름" },
   { key: "userId", value: "학번" },
-  { key: "group", value: "구분" },
+  { key: "userGroup", value: "구분" },
   { key: "phone", value: "전화번호" },
   { key: "lastSemester", value: "마지막 재학 학기" },
   { key: "edit", value: "수정" },
   { key: "select", value: "선택" }
   ]}
-  rows={[
-    {
-      id: "20211561",
-      userName: "구효민",
-      userId: "20211561",
-      group: "관리자",
-      phone: "01012345678",
-      lastSemester: "2022-1",
-      edit: ""
-    }
-  ]}
+  rows={users}
 >
   <svelte:fragment slot="cell" let:row let:cell>
     {#if cell.key === "edit"}
       <Button>수정</Button>
+    {:else if cell.key === "select"}
+      <Checkbox labelText="선택" />
     {:else}
       {cell.value}
     {/if}
