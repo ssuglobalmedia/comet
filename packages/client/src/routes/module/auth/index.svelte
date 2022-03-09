4944173b71813971d74e4545053b14de4b4d800c
EATSTEAK <eatsteak95@gmail.com>
2022-03-10 07:12:40 +0900

    feat: make table header, actions and skeleton

--- packages/client/src/routes/module/auth/index.svelte
+++ packages/client/src/routes/module/auth/index.svelte
@@ -1,61 +1,75 @@
 <script lang="ts">
-  import { Button, Checkbox, Column, DataTable, Grid } from "carbon-components-svelte";
+  import {
+    Button,
+    DataTable,
+    DataTableSkeleton,
+    Toolbar, ToolbarBatchActions, ToolbarContent,
+    ToolbarSearch
+  } from "carbon-components-svelte";
   import { browser } from "$app/env";
   import { variables } from "$lib/variables";
   import { fetchWithAuth, groupDisplayName } from "$lib/module/auth";
-  import type {CometResponse, User} from "mirinae-comet";
+  import type { CometResponse, User } from "mirinae-comet";
+  import { Delete16 } from "carbon-icons-svelte";
 
   type UserCell = {
     id: string;
   } & User;
 
-  let users: Array<UserCell> = [];
+  const headers = [
+    { key: "userName", value: "사용자 이름" },
+    { key: "userId", value: "학번" },
+    { key: "userGroup", value: "구분" },
+    { key: "phone", value: "전화번호" },
+    { key: "lastSemester", value: "마지막 재학 학기" },
+    { key: "edit", value: "수정" }
+  ];
+  let users: Array<UserCell> = undefined;
+
+  let selectedUsers = [];
 
   const transformUser = (user: User): UserCell => ({
     id: user.userId,
     ...user,
-    userGroup: groupDisplayName[user.userGroup] ?? '알 수 없음'
-  })
+    userGroup: groupDisplayName[user.userGroup] ?? "알 수 없음"
+  });
 
-  if(browser) {
+  if (browser) {
     fetchWithAuth(`${variables.baseUrl as string}/api/module/auth/user/query`).then((res) => res.json()).then((res: CometResponse) => {
-      if(!res.success) throw new Error(`Request error ${res.error}: ${res.error_description}`);
+      if (!res.success) throw new Error(`Request error ${res.error}: ${res.error_description}`);
       users = (res.result as Array<User>).map(transformUser);
     }).catch((err) => {
       console.error(err);
     });
   }
 </script>
-
-<Grid noGutter condensed>
-  <Column>
-    <h1>사용자 목록</h1>
-  </Column>
-  <Column>
-    <Button>XLSX로 내보내기</Button>
-    <Button href="/module/auth/upload">파일으로부터 업로드</Button>
-  </Column>
-</Grid>
-
-<DataTable
-  headers={[
-  { key: "userName", value: "사용자 이름" },
-  { key: "userId", value: "학번" },
-  { key: "userGroup", value: "구분" },
-  { key: "phone", value: "전화번호" },
-  { key: "lastSemester", value: "마지막 재학 학기" },
-  { key: "edit", value: "수정" },
-  { key: "select", value: "선택" }
-  ]}
-  rows={users}
->
-  <svelte:fragment slot="cell" let:row let:cell>
-    {#if cell.key === "edit"}
-      <Button>수정</Button>
-    {:else if cell.key === "select"}
-      <Checkbox labelText="선택" />
-    {:else}
-      {cell.value}
-    {/if}
-  </svelte:fragment>
-</DataTable>
+{#if users}
+  <DataTable
+    batchSelection
+    bind:selectedRowIds={selectedUsers}
+    title="학부생 목록"
+    description="학부생 목록을 열람하고 관리합니다."
+    {headers}
+    rows={users}
+  >
+    <Toolbar>
+      <ToolbarBatchActions>
+        <Button icon={Delete16}>삭제</Button>
+      </ToolbarBatchActions>
+      <ToolbarContent>
+        <ToolbarSearch />
+        <Button>XLSX로 내보내기</Button>
+        <Button href="/module/auth/upload">파일으로부터 업로드</Button>
+      </ToolbarContent>
+    </Toolbar>
+    <svelte:fragment slot="cell" let:row let:cell>
+      {#if cell.key === "edit"}
+        <Button>수정</Button>
+      {:else}
+        {cell.value}
+      {/if}
+    </svelte:fragment>
+  </DataTable>
+{:else}
+  <DataTableSkeleton {headers} />
+{/if}
