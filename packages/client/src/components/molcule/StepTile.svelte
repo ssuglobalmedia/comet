31795de18f36ad0cdc9d310176446678f4fd720b
EATSTEAK <eatsteak95@gmail.com>
2022-03-11 23:17:16 +0900

    feat: fully-working upload page with DataTransformer

---
+++ packages/client/src/components/molcule/StepTile.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Column, Grid, Tile } from "carbon-components-svelte";
+  export let title;
+  export let description;
+</script>
+
+<Tile>
+  <h1>{title}</h1>
+  <p>{description}</p>
+  <div class="my-4">
+    <slot />
+    <Grid noGutter condensed>
+      <Column style="display: flex; justify-content: end;">
+        <slot name="navigation" />
+      </Column>
+    </Grid>
+  </div>
+</Tile>
