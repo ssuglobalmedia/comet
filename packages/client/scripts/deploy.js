83d72489b6ed193a38358aa8a61acc779424c9da
EATSTEAK <me@eatsteak.dev>
2022-03-13 03:17:03 +0900

    fix: remove .html extension on s3 files for proper navigation

--- packages/client/scripts/deploy.js
+++ packages/client/scripts/deploy.js
@@ -1,5 +1,6 @@
 import { exec } from 'child_process';
 
+// Getting s3 bucket information.
 exec('aws cloudformation describe-stacks --stack-name mirinae-comet', (err, stdout, stderr) => {
 	if (err) {
 		console.error(`Exception thrown: ${err.message}`);
@@ -18,6 +19,7 @@ exec('aws cloudformation describe-stacks --stack-name mirinae-comet', (err, stdo
 		return;
 	}
 	console.log(`Found s3 bucket: ${s3Name}`);
+	// Upload local files to s3 bucket.
 	exec(`aws s3 sync ./build "s3://${s3Name}/" --delete`, (err, stdout, stderr) => {
 		if (err) {
 			console.error(`Exception thrown: ${err.message}`);
@@ -28,5 +30,40 @@ exec('aws cloudformation describe-stacks --stack-name mirinae-comet', (err, stdo
 			return;
 		}
 		console.log(`${stdout}`);
+		console.log('File uploaded, renaming html...');
+		exec(`aws s3api list-objects --bucket ${s3Name}`, (err, stdout, stderr) => {
+			if (err) {
+				console.error(`Exception thrown: ${err.message}`);
+				return;
+			}
+			if (stderr) {
+				console.error(`Error occurred: ${stderr}`);
+				return;
+			}
+			const files = JSON.parse(stdout);
+			const contents = files['Contents'];
+			contents.forEach((content) => {
+				const key = content['Key'];
+				if (key.endsWith('.html') && !key.endsWith('index.html')) {
+					exec(
+						`aws s3 mv s3://${s3Name}/${key} s3://${s3Name}/${key.substr(
+							0,
+							key.lastIndexOf('.html')
+						)}`,
+						(err, stdout, stderr) => {
+							if (err) {
+								console.error(`Exception thrown: ${err.message}`);
+								return;
+							}
+							if (stderr) {
+								console.error(`Error occurred: ${stderr}`);
+								return;
+							}
+							console.log(`${stdout}`);
+						}
+					);
+				}
+			});
+		});
 	});
 });
