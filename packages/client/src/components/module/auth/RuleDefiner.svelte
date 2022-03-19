2ba7cdd3e42c13b364d31b6b31f4c691be3f02a3
Hyomin Koo <me@eatsteak.dev>
2022-03-20 06:09:03 +0900

    feat: implement auth check page

---
+++ packages/client/src/components/module/auth/RuleDefiner.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import {Checkbox, Dropdown, FormGroup} from "carbon-components-svelte";
+
+  export let originalData: Array<Record<string, any>>;
+  export let rule;
+
+  $: dropdownItems = originalData ? [
+    ...Object.keys(originalData[0]).filter((k) => (k !== "__rowNum__")).map((k, idx) => ({
+      id: idx, text: k
+    }))] : [];
+  let userIdCol = 0;
+  let userNameCol = 0;
+  let checkPhone = false;
+  let checkCertificated = false;
+  let checkLastSemester = false;
+  let phoneCol = 0;
+
+  function formValidation(): boolean {
+    const set = new Set<number>();
+    set.add(userIdCol);
+    set.add(userNameCol);
+    if(checkPhone) set.add(phoneCol);
+    if (set.size < (3 + (checkPhone ? 1 : 0))) return false;
+    return true;
+  }
+
+  $: {
+    if(formValidation()) {
+      rule = {
+        userId: dropdownItems[userIdCol].text,
+        userName: dropdownItems[userNameCol].text,
+        checkCertificated,
+        checkPhone,
+        ...(checkPhone && { phone: dropdownItems[phoneCol].text }),
+        checkLastSemester
+      }
+    } else {
+      rule = undefined;
+    }
+  }
+</script>
+
+<h4>규칙 설정</h4>
+<p class="bx--data-table-header__description">검사할 데이터의 검사 규칙을 선택해 주세요.</p>
+<div style="margin-top: 1.5rem">
+  <FormGroup legendText="행 선택">
+    <Dropdown
+        titleText="학번"
+        bind:selectedId={userIdCol}
+        items={dropdownItems}
+        invalid={userIdCol === userNameCol}
+        invalidText="속성에 대해 선택한 행이 다른 속성과 겹칩니다."
+    />
+    <Dropdown
+        titleText="사용자 이름"
+        bind:selectedId={userNameCol}
+        items={dropdownItems}
+        invalid={userNameCol === userIdCol}
+        invalidText="속성에 대해 선택한 행이 다른 속성과 겹칩니다."
+    />
+    <div style="display: flex; align-items: center;">
+      <Checkbox style="flex: 0 1 auto; margin-right: 0.875rem;" labelText="전화번호 확인" hideLabel
+                bind:checked={checkPhone}/>
+      <Dropdown
+          style="flex: 1 0 auto;"
+          titleText="전화번호"
+          disabled={!checkPhone}
+          bind:selectedId={phoneCol}
+          items={dropdownItems}
+          invalid={checkPhone && (phoneCol === userIdCol || phoneCol === userNameCol)}
+          invalidText="속성에 대해 선택한 행이 다른 속성과 겹칩니다."
+      />
+    </div>
+  </FormGroup>
+  <FormGroup legendText="규칙 선택">
+    <Checkbox labelText="납부자 여부 확인" bind:checked={checkCertificated} />
+    <Checkbox labelText="재학 여부 확인" bind:checked={checkLastSemester} />
+  </FormGroup>
+</div>
---
+++ packages/client/src/routes/module/auth/check.svelte
@@ -0,0 +1,184 @@
+<script lang="ts">
+  import {
+    Button,
+    DataTable,
+    DataTableSkeleton,
+    FileUploader,
+    Pagination,
+    ProgressIndicator,
+    ProgressStep
+  } from "carbon-components-svelte";
+  import type {WorkBook} from "xlsx";
+  import {read, utils} from "xlsx";
+  import {NextFilled16, TaskComplete16, WarningAlt16, CheckmarkFilled16} from "carbon-icons-svelte";
+  import StepTile from "../../../components/molcule/StepTile.svelte";
+  import type {CometResponse} from "@types/mirinae-comet";
+  import {User} from "@types/mirinae-comet";
+  import {fetchWithAuth, isAccessible} from "$lib/module/auth";
+  import {browser} from "$app/env";
+  import {variables} from "$lib/variables";
+  import RuleDefiner from "../../../components/module/auth/RuleDefiner.svelte";
+  import {getCurrentFullSemester} from "$lib/utils";
+
+  let files: Array<File> = [];
+  let workbook: WorkBook = undefined;
+  let data: Array<Record<string, any>> = undefined;
+  $: previewHeader = data ? Object.keys(data[0]).filter((k) => (k !== "__rowNum__")).map((k) => ({
+    key: k, value: k
+  })) : [];
+  $: previewData = data ? data.slice(0, 5).map((v) => ({
+    id: v.__rowNum__,
+    ...v
+  })) : [];
+  let error = undefined;
+  let currentIndex = 0;
+  $: if (files.length && !workbook) {
+    files[0].arrayBuffer().then((buf) => {
+      try {
+        workbook = read(buf, {type: "buffer"});
+        data = utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+      } catch (e) {
+        error = e;
+        console.error(e);
+      }
+    });
+  }
+
+  let validationRule;
+  let validatedData;
+
+  $: if (browser && currentIndex === 2 && validationRule && !validatedData) {
+    (async () => {
+      const res = await (await fetchWithAuth(variables.baseUrl + "/api/module/auth/user/query")).json() as CometResponse;
+      if (res.success) {
+        const users: Array<User> = (res.result as Array<User>);
+        validatedData = data.reduce((arr, v) => {
+          const userId = v[validationRule.userId];
+          const userName = v[validationRule.userName];
+          const exactUser = users.find((v) => v.userId === userId && v.userName === userName);
+          const foundUser = users.filter((v) => v.userId === userId || v.userName === userName);
+          const problems = [];
+          if (!exactUser) {
+            if (foundUser.length) {
+              const sameIds = foundUser.filter((v) => v.userId === userId);
+              const sameNames = foundUser.filter((v) => v.userName === userName);
+              if(sameIds.length) {
+                problems.push({ id: "mismatch_name", fixes: sameIds.map((v) => v.userName)});
+              }
+              if(sameNames.length) {
+                problems.push({ id: "mismatch_id", fixes: sameNames.map((v) => v.userId)});
+              }
+            } else {
+              problems.push({ id: "no_user" });
+            }
+          } else {
+            if(validationRule.checkCertificated) {
+              if(!isAccessible(exactUser.userGroup, 'certificated')) problems.push({ id: "no_permission" });
+            }
+            if(validationRule.checkLastSemester) {
+              if(exactUser.lastSemester !== getCurrentFullSemester()) problems.push({ id: "not_attended" });
+            }
+          }
+          return [...arr, {
+            id: userId,
+            userId,
+            userName,
+            problem: problems
+          }];
+        }, []);
+      }
+    })();
+  }
+
+  const validationHeaders = [
+    {key: "userId", value: "학번"},
+    {key: "userName", value: "사용자 이름"},
+    {key: "problem", value: "문제"}
+  ];
+
+  let validationTablePageSize = 25;
+  let validationTablePage = 1;
+</script>
+
+<ProgressIndicator spaceEqually class="my-4" bind:currentIndex>
+  <ProgressStep complete={workbook} label="파일 업로드"/>
+  <ProgressStep complete={validationRule !== undefined} disabled={!workbook} label="데이터 확인"/>
+  <ProgressStep complete={currentIndex > 2} disabled={!validationRule} label="문제 확인"/>
+</ProgressIndicator>
+{#if currentIndex === 0}
+  <StepTile
+      title="파일 업로드"
+      description="csv 혹은 xls, xlsx 파일을 업로드하세요. 첫번째 열은 제목으로 간주되며, 여러 시트가 있을 경우 첫번째 시트만 인식합니다."
+  >
+    <p style="color: var(--cds-danger-02)">이미 존재하는 사용자의 정보를 업로드할 경우 기존 정보가 전부 덮어씌워지므로 주의하세요!</p>
+    <FileUploader
+        buttonLabel="업로드"
+        accept={[".csv", ".xls", ".xlsx"]}
+        bind:files
+        status={workbook ? "complete" : error ? "edit" : "loading"}
+        on:change={() => {
+          workbook = undefined;
+          data = undefined;
+          error = undefined;
+        }}
+    />
+    <Button slot="navigation" on:click={() => {if(currentIndex < 1) currentIndex = 1}} icon={NextFilled16}
+            disabled={!workbook}>다음으로
+    </Button>
+  </StepTile>
+{:else if currentIndex === 1}
+  <StepTile
+      title="데이터 확인"
+      description="업로드한 데이터를 확인하고 변환하세요."
+  >
+
+    <DataTable
+        title="데이터 확인"
+        description="업로드한 데이터의 샘플(첫 5개)을 확인합니다."
+        headers={previewHeader}
+        rows={previewData}
+    />
+    <RuleDefiner originalData={data} bind:rule={validationRule}/>
+    <Button slot="navigation" on:click={() => {if(currentIndex < 2) currentIndex = 2}} icon={NextFilled16}
+            disabled={!validationRule}>다음으로
+    </Button>
+  </StepTile>
+{:else}
+  <StepTile
+      title="문제 확인"
+      description='전체 데이터를 검증하고 문제를 확인합니다.'
+  >
+    {#if validatedData}
+      <DataTable
+          sortable
+          title="데이터 확인"
+          description="데이터를 확인합니다."
+          headers={validationHeaders}
+          rows={validatedData ?? []}
+          pageSize={validationTablePageSize}
+          page={validationTablePage}
+      >
+        <svelte:fragment slot="cell" let:row let:cell>
+          {#if cell.key === "problem"}
+            {#if cell.value.length}
+              <WarningAlt16 />
+            {:else}
+              <CheckmarkFilled16 />
+            {/if}
+          {:else}
+            {cell.value ?? "정보 없음"}
+          {/if}
+        </svelte:fragment>
+      </DataTable>
+    {:else}
+      <DataTableSkeleton headers={validationHeaders}/>
+    {/if}
+    <Pagination
+        bind:pageSize={validationTablePageSize}
+        bind:page={validationTablePage}
+        totalItems={(validatedData ?? []).length}
+        pageSizeInputDisabled
+    />
+    <Button slot="navigation" on:click={() => {if(currentIndex < 3) currentIndex = 3}} icon={TaskComplete16}>완료</Button>
+  </StepTile>
+{/if}
