00a4997bfd2de4531bf9b958a99e93ebde33046a
Hyomin Koo <me@eatsteak.dev>
2022-03-20 04:06:09 +0900

    feat: implement export, batch delete

--- packages/client/src/routes/module/auth/index.svelte
+++ packages/client/src/routes/module/auth/index.svelte
@@ -2,34 +2,39 @@
   import {
     Button,
     DataTable,
-    DataTableSkeleton, Pagination, PaginationSkeleton,
-    Toolbar, ToolbarBatchActions, ToolbarContent,
+    DataTableSkeleton,
+    Pagination,
+    PaginationSkeleton,
+    Toolbar,
+    ToolbarBatchActions,
+    ToolbarContent,
     ToolbarSearch
   } from "carbon-components-svelte";
-  import { browser } from "$app/env";
-  import { variables } from "$lib/variables";
-  import { userInfo } from "$lib/stores";
-  import { fetchWithAuth, groupDisplayName } from "$lib/module/auth";
-  import type { CometResponse, User } from "mirinae-comet";
-  import { Delete16, Download16, Edit16, Upload16 } from "carbon-icons-svelte";
+  import {browser} from "$app/env";
+  import {variables} from "$lib/variables";
+  import {userInfo} from "$lib/stores";
+  import {fetchWithAuth, groupDisplayName} from "$lib/module/auth";
+  import type {CometResponse, User} from "mirinae-comet";
+  import {Checkmark16, Delete16, Edit16, Export16, Upload16} from "carbon-icons-svelte";
   import UpdateModal from "../../../components/module/auth/UpdateModal.svelte";
   import BatchUpdateModal from "../../../components/module/auth/BatchUpdateModal.svelte";
+  import ExportModal from "../../../components/module/auth/ExportModal.svelte";
 
   type UserCell = {
     id: string;
   } & User;
 
   const headers = [
-    { key: "userId", value: "학번" },
-    { key: "userName", value: "사용자 이름" },
-    { key: "userGroup", value: "구분" },
-    { key: "phone", value: "전화번호" },
-    { key: "lastSemester", value: "마지막 재학 학기" },
-    { key: "edit", value: "수정", empty: true }
+    {key: "userId", value: "학번"},
+    {key: "userName", value: "사용자 이름"},
+    {key: "userGroup", value: "구분"},
+    {key: "phone", value: "전화번호"},
+    {key: "lastSemester", value: "마지막 재학 학기"},
+    {key: "edit", value: "수정", empty: true}
   ];
   let users: Array<UserCell> = undefined;
 
-  let selectedUsers: Array<UserCell> = [];
+  let selectedUsers: Array<string> = [];
 
   const transformUser = (user: User): UserCell => ({
     id: user.userId,
@@ -38,18 +43,14 @@
   });
 
   if (browser) {
-    fetchWithAuth(`${variables.baseUrl as string}/api/module/auth/user/query`).then((res) => res.json()).then((res: CometResponse) => {
-      if (!res.success) throw new Error(`Request error ${res.error}: ${res.error_description}`);
-      users = (res.result as Array<User>).map(transformUser);
-    }).catch((err) => {
-      console.error(err);
-    });
+    updateUsers();
   }
 
   let pageSize = 25;
   let page = 1;
 
   let debouncer;
+
   function debounce(func: () => void) {
     clearTimeout(debouncer);
     debouncer = setTimeout(() => {
@@ -59,20 +60,30 @@
 
   let searchValue;
   let debouncedSearchValue;
-  $: if(searchValue || debouncedSearchValue) {
+  $: if (searchValue || debouncedSearchValue) {
     debounce(() => {
       debouncedSearchValue = searchValue;
     });
   }
 
   $: filteredUsers = users ? users.filter((v) => {
-    if(!debouncedSearchValue) return true;
+    if (!debouncedSearchValue) return true;
     return `${v.userId}`.includes(debouncedSearchValue) || v.userName.includes(debouncedSearchValue) || (groupDisplayName[v.userGroup ?? 'unregistered'] ?? '').includes(debouncedSearchValue) || (v.lastSemester ?? '').includes(debouncedSearchValue) || `${v.phone ?? ''}`.includes(debouncedSearchValue);
   }) : [];
 
   let updateModalOpen = false;
   let batchUpdateModalOpen = false;
   let updateTargetUser: User = undefined;
+
+  function updateUsers() {
+    fetchWithAuth(`${variables.baseUrl as string}/api/module/auth/user/query`).then((res) => res.json()).then((res: CometResponse) => {
+      if (!res.success) throw new Error(`Request error ${res.error}: ${res.error_description}`);
+      users = (res.result as Array<User>).map(transformUser);
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+
   function openUpdateModal(row) {
     updateTargetUser = row;
     updateModalOpen = true;
@@ -81,33 +92,53 @@
   function openBatchUpdateModal() {
     batchUpdateModalOpen = true;
   }
+
+  function deleteSelected() {
+    fetchWithAuth(variables.baseUrl + "/api/module/auth/user/batch/delete",
+      {
+        method: "POST",
+        cache: "no-cache",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(selectedUsers)
+      }).then((res) => res.json()).then(() => {
+        selectedUsers = [];
+        updateUsers();
+    })
+  }
+
+  let exportModalOpen = false;
 </script>
 {#if users}
   <DataTable
-    sortable
-    batchSelection
-    bind:selectedRowIds={selectedUsers}
-    title="학부생 목록"
-    description="학부생 목록을 열람하고 관리합니다."
-    {headers}
-    rows={filteredUsers}
-    pageSize={pageSize}
-    page={page}
+      sortable
+      batchSelection
+      bind:selectedRowIds={selectedUsers}
+      title="학부생 목록"
+      description="학부생 목록을 열람하고 관리합니다."
+      {headers}
+      rows={filteredUsers}
+      pageSize={pageSize}
+      page={page}
   >
     <Toolbar>
       <ToolbarBatchActions>
         <Button icon={Edit16} on:click={openBatchUpdateModal} disabled={$userInfo.userGroup !== "admin"}>일괄 수정</Button>
-        <Button icon={Delete16} disabled={$userInfo.userGroup !== "admin"}>삭제</Button>
+        <Button icon={Delete16} on:click={deleteSelected} disabled={$userInfo.userGroup !== "admin"}>삭제</Button>
       </ToolbarBatchActions>
       <ToolbarContent>
-        <ToolbarSearch bind:value={searchValue} />
-        <Button icon={Download16}>XLSX로 내보내기</Button>
-        <Button href="/module/auth/upload" icon={Upload16} disabled={$userInfo.userGroup !== "admin"}>파일으로부터 업로드</Button>
+        <ToolbarSearch bind:value={searchValue}/>
+        <Button on:click={() => { exportModalOpen = true; }} icon={Export16}>XLSX로 내보내기</Button>
+        <Button href="/module/auth/check" icon={Checkmark16}>납부자 명단 확인</Button>
+        <Button kind="secondary" href="/module/auth/upload" icon={Upload16} disabled={$userInfo.userGroup !== "admin"}>파일으로부터 업로드
+        </Button>
       </ToolbarContent>
     </Toolbar>
     <svelte:fragment slot="cell" let:row let:cell>
       {#if cell.key === "edit"}
-        <Button icon={Edit16} on:click={() => openUpdateModal(row)} disabled={$userInfo.userGroup !== "admin"}>수정</Button>
+        <Button icon={Edit16} on:click={() => openUpdateModal(row)} disabled={$userInfo.userGroup !== "admin"}>수정
+        </Button>
       {:else if cell.key === "userGroup"}
         {groupDisplayName[cell.value] ?? cell.value}
       {:else}
@@ -116,14 +147,16 @@
     </svelte:fragment>
   </DataTable>
   <Pagination
-    pageSizes={[25, 50, 100, 200]}
-    bind:pageSize={pageSize}
-    bind:page={page}
-    totalItems={(filteredUsers).length}
+      pageSizes={[25, 50, 100, 200]}
+      bind:pageSize={pageSize}
+      bind:page={page}
+      totalItems={(filteredUsers).length}
   />
 {:else}
-  <DataTableSkeleton {headers} />
-  <PaginationSkeleton />
+  <DataTableSkeleton {headers}/>
+  <PaginationSkeleton/>
 {/if}
-<UpdateModal bind:open={updateModalOpen} bind:targetUser={updateTargetUser} on:close={() => (updateModalOpen = false)} />
-<BatchUpdateModal bind:open={batchUpdateModalOpen} userSupplier={users} selectedUserIds={selectedUsers} on:close={() => (batchUpdateModalOpen = false)} />
+<ExportModal bind:open={exportModalOpen} bind:users={users} on:close={() => (exportModalOpen = false)}/>
+<UpdateModal bind:open={updateModalOpen} bind:targetUser={updateTargetUser} on:close={() => (updateModalOpen = false)}/>
+<BatchUpdateModal bind:open={batchUpdateModalOpen} userSupplier={users} selectedUserIds={selectedUsers}
+                  on:close={() => (batchUpdateModalOpen = false)}/>
