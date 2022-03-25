8365b94997a86d5add72b0aed0d26ed7a12da351
EATSTEAK <me@eatsteak.dev>
2022-03-25 10:17:17 +0900

    fix: layout on mobile

--- packages/client/src/routes/module/auth/index.svelte
+++ packages/client/src/routes/module/auth/index.svelte
@@ -5,7 +5,7 @@
     PaginationSkeleton,
     Toolbar,
     ToolbarBatchActions,
-    ToolbarContent,
+    ToolbarContent, ToolbarMenu, ToolbarMenuItem,
     ToolbarSearch
   } from "carbon-components-svelte";
   import { browser } from "$app/env";
@@ -13,7 +13,7 @@
   import { userInfo } from "$lib/stores";
   import { fetchWithAuth, groupDisplayName } from "$lib/module/auth";
   import type { CometResponse, User } from "mirinae-comet";
-  import { Checkmark16, Delete16, Edit16, Export16, Upload16 } from "carbon-icons-svelte";
+  import { Checkmark16, Delete16, Edit16 } from "carbon-icons-svelte";
   import UpdateModal from "../../../components/molcule/module/auth/UpdateModal.svelte";
   import BatchUpdateModal from "../../../components/molcule/module/auth/BatchUpdateModal.svelte";
   import ExportModal from "../../../components/molcule/module/auth/ExportModal.svelte";
@@ -109,55 +109,59 @@
 
   let exportModalOpen = false;
 </script>
-{#if users}
-  <DataTable
-    zebra
-    sortable
-    batchSelection
-    bind:selectedRowIds={selectedUsers}
-    title="학부생 목록"
-    description="학부생 목록을 열람하고 관리합니다."
-    {headers}
-    rows={filteredUsers}
-    pageSize={pageSize}
-    page={page}
-  >
-    <Toolbar>
-      <ToolbarBatchActions formatTotalSelected={(totalSelected) => `${totalSelected}개 선택됨`}>
-        <svelte:fragment slot="cancel">선택 해제</svelte:fragment>
-        <Button icon={Edit16} on:click={openBatchUpdateModal} disabled={$userInfo.userGroup !== "admin"}>일괄 수정</Button>
-        <Button icon={Delete16} on:click={deleteSelected} disabled={$userInfo.userGroup !== "admin"}>삭제</Button>
-      </ToolbarBatchActions>
-      <ToolbarContent>
-        <ToolbarSearch bind:value={searchValue} placeholder="검색하기..." />
-        <Button on:click={() => { exportModalOpen = true; }} icon={Export16}>XLSX로 내보내기</Button>
-        <Button href="/module/auth/check" icon={Checkmark16}>사용자 명단 확인</Button>
-        <Button kind="secondary" href="/module/auth/upload" icon={Upload16}
-                disabled={$userInfo.userGroup !== "admin"}>파일으로부터 업로드
-        </Button>
-      </ToolbarContent>
-    </Toolbar>
-    <svelte:fragment slot="cell" let:row let:cell>
-      {#if cell.key === "edit"}
-        <Button iconDescription="수정" kind="ghost" icon={Edit16} on:click={() => openUpdateModal(row)}
-                disabled={$userInfo.userGroup !== "admin"} />
-      {:else if cell.key === "userGroup"}
-        {groupDisplayName[cell.value] ?? cell.value}
-      {:else}
-        {cell.value ?? "정보 없음"}
-      {/if}
-    </svelte:fragment>
-  </DataTable>
-  <PaginationKor
-    pageSizes={[25, 50, 100, 200, 500]}
-    bind:pageSize={pageSize}
-    bind:page={page}
-    totalItems={(filteredUsers).length}
-  />
-{:else}
-  <DataTableSkeleton {headers} />
-  <PaginationSkeleton />
-{/if}
+<div style="min-width: 400px;">
+  {#if users}
+    <DataTable
+      sortable
+      batchSelection
+      bind:selectedRowIds={selectedUsers}
+      title="학부생 목록"
+      description="학부생 목록을 열람하고 관리합니다."
+      {headers}
+      rows={filteredUsers}
+      pageSize={pageSize}
+      page={page}
+    >
+      <Toolbar>
+        <ToolbarBatchActions formatTotalSelected={(totalSelected) => `${totalSelected}개 선택됨`}>
+          <svelte:fragment slot="cancel">선택 해제</svelte:fragment>
+          <Button icon={Edit16} on:click={openBatchUpdateModal} disabled={$userInfo.userGroup !== "admin"}>일괄 수정</Button>
+          <Button icon={Delete16} on:click={deleteSelected} disabled={$userInfo.userGroup !== "admin"}>삭제</Button>
+        </ToolbarBatchActions>
+        <ToolbarContent>
+          <ToolbarSearch bind:value={searchValue} placeholder="검색하기..." />
+          <Button href="/module/auth/check" icon={Checkmark16}>명단 확인</Button>
+          <ToolbarMenu>
+            <ToolbarMenuItem on:click={() => { exportModalOpen = true; }} primaryFocus>XLSX로 내보내기</ToolbarMenuItem>
+            <ToolbarMenuItem href="/module/auth/upload" disabled={$userInfo.userGroup !== "admin"}>
+              파일 업로드
+            </ToolbarMenuItem>
+          </ToolbarMenu>
+        </ToolbarContent>
+      </Toolbar>
+      <svelte:fragment slot="cell" let:row let:cell>
+        {#if cell.key === "edit"}
+          <Button iconDescription="수정" kind="ghost" icon={Edit16} on:click={() => openUpdateModal(row)}
+                  disabled={$userInfo.userGroup !== "admin"} />
+        {:else if cell.key === "userGroup"}
+          {groupDisplayName[cell.value] ?? cell.value}
+        {:else}
+          {cell.value ?? "정보 없음"}
+        {/if}
+      </svelte:fragment>
+    </DataTable>
+    <PaginationKor
+      pageSizes={[25, 50, 100, 200, 500]}
+      bind:pageSize={pageSize}
+      bind:page={page}
+      totalItems={(filteredUsers).length}
+    />
+  {:else}
+    <DataTableSkeleton {headers} />
+    <PaginationSkeleton />
+  {/if}
+</div>
+
 <ExportModal bind:open={exportModalOpen} bind:users={users} on:close={() => (exportModalOpen = false)} />
 <UpdateModal bind:open={updateModalOpen} bind:targetUser={updateTargetUser}
              on:close={() => (updateModalOpen = false)} />
