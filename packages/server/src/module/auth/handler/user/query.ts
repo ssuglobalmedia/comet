d3c684a1d828e7d253fa9b3c82d3701cd0ac5fd0
EATSTEAK <me@eatsteak.dev>
2022-03-27 01:03:21 +0900

    feat: add `starts` query parameter to user query api

--- packages/server/src/module/auth/handler/user/query.ts
+++ packages/server/src/module/auth/handler/user/query.ts
@@ -8,12 +8,13 @@ import { ResponsibleError } from '../../../../util/error';
 import { assertAccessible } from '../../util/permission';
 
 export const userQueryHandler: APIGatewayProxyHandler = async (event) => {
+	const startsWith = event.queryStringParameters?.starts ?? '';
 	const token = (event.headers.Authorization ?? '').replace('Bearer ', '');
 	try {
 		const id = (jwt.verify(token, JWT_SECRET) as JwtPayload).aud as string;
 		await assertAccessible(id, token, 'executive');
 		console.log(event);
-		const result = await queryUser('');
+		const result = await queryUser(startsWith);
 		return createResponse(200, {
 			success: true,
 			result
