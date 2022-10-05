43f082e33990dbaad3baf68f33e45c253d45d12d
Hyomin Koo <me@eatsteak.dev>
2022-10-05 15:52:11 +0900

    feat(config): create handler code and sam setup for config endpoints

---
+++ packages/server/src/module/config/handler/get.ts
@@ -0,0 +1,32 @@
+import type { APIGatewayProxyHandler } from 'aws-lambda';
+import * as jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../../../env';
+import type { JwtPayload } from 'jsonwebtoken';
+import { createResponse } from '../../../common';
+import {
+  InternalError,
+  isCometError,
+  responseAsCometError,
+  UnauthorizedError,
+} from '../../../util/error';
+import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
+import { getConfig } from '../data/config';
+
+export const configGetHandler: APIGatewayProxyHandler = async (event) => {
+  const token = (event.headers.Authorization ?? '').replace('Bearer ', '');
+  try {
+    const id = (jwt.verify(token, JWT_SECRET) as JwtPayload).aud as string;
+    const result = await getConfig();
+    return createResponse(200, {
+      success: true,
+      result,
+    });
+  } catch (e) {
+    if (isCometError(e)) return responseAsCometError(e);
+    if (e instanceof JsonWebTokenError || e instanceof TokenExpiredError) {
+      return responseAsCometError(new UnauthorizedError());
+    }
+    console.error(e);
+    return responseAsCometError(new InternalError());
+  }
+};
---
+++ packages/server/src/module/config/handler/update.ts
@@ -0,0 +1,45 @@
+import type { APIGatewayProxyHandler } from 'aws-lambda';
+import { createResponse } from '../../../common';
+import type { JwtPayload } from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../../../env';
+import { assertAccessible } from '../../auth/util/permission';
+import { updateGoods } from '../data/rental';
+import {
+  BadRequestError,
+  CometError,
+  InternalError,
+  responseAsCometError,
+  UnauthorizedError,
+} from '../../../util/error';
+import type { GoodsUpdateRequest } from 'mirinae-comet';
+import { updateConfig } from '../data/config';
+
+export const configUpdateHandler: APIGatewayProxyHandler = async (event) => {
+  const token = (event.headers.Authorization ?? '').replace('Bearer ', '');
+  let data: GoodsUpdateRequest;
+  try {
+    data = JSON.parse(event.body) as GoodsUpdateRequest;
+  } catch {
+    return responseAsCometError(new BadRequestError('Data body is malformed JSON'));
+  }
+  if (!data || !data.id) {
+    return responseAsCometError(new BadRequestError());
+  }
+  let payload: JwtPayload;
+  try {
+    payload = jwt.verify(token, JWT_SECRET) as JwtPayload;
+  } catch {
+    console.debug('malformed token');
+    return responseAsCometError(new UnauthorizedError());
+  }
+  try {
+    const id = payload.aud as string;
+    await assertAccessible(id, token, 'executive');
+    const res = await updateConfig(data);
+    return createResponse(200, { success: res });
+  } catch (e) {
+    console.error(e);
+    return responseAsCometError(new InternalError());
+  }
+};
