e62bbe67acf4a7fb68bd5c5d87ee3579d71b3e0e
Hyomin Koo <me@eatsteak.dev>
2022-10-05 15:55:05 +0900

    chore(common): cache CORS preflight in local environment for faster development

--- packages/server/src/common.ts
+++ packages/server/src/common.ts
@@ -1,29 +1,36 @@
 import type { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
 
 export function createResponse(statusCode: number, body: string | object): APIGatewayProxyResult {
-	const stringifyBody = typeof body === 'string' ? body : JSON.stringify(body);
-	const res: APIGatewayProxyResult = {
-		statusCode,
-		body: stringifyBody,
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	};
-	if (process.env.AWS_SAM_LOCAL) {
-		res.headers['Access-Control-Allow-Origin'] = '*';
-	}
-	return res;
+  const stringifyBody = typeof body === 'string' ? body : JSON.stringify(body);
+  const res: APIGatewayProxyResult = {
+    statusCode,
+    body: stringifyBody,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  if (process.env.AWS_SAM_LOCAL) {
+    res.headers['Access-Control-Allow-Origin'] = '*';
+  }
+  return res;
 }
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export const localCorsHandler: APIGatewayProxyHandler = async () => {
-	return {
-		statusCode: 200,
-		headers: {
-			'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key',
-			'Access-Control-Allow-Methods': 'POST, GET',
-			'Access-Control-Allow-Origin': '*'
-		},
-		body: ''
-	};
+  if (!process.env.AWS_SAM_LOCAL) {
+    return {
+      statusCode: 200,
+      body: '',
+    };
+  }
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key',
+      'Access-Control-Allow-Methods': 'POST, GET',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Max-Age': '86400',
+    },
+    body: '',
+  };
 };
