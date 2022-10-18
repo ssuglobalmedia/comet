8abf2b85300932be4abf0882c70c665d1c2ee569
Hyomin Koo <me@eatsteak.dev>
2022-10-18 12:55:09 +0900

    chore: cleanup

--- packages/client/src/components/molcule/module/dashboard/ModuleDashboard.svelte
+++ packages/client/src/components/molcule/module/dashboard/ModuleDashboard.svelte
@@ -3,11 +3,6 @@
   import { ClickableTile, Column, Grid, Row } from 'carbon-components-svelte';
   import { isAccessible, groupDisplayName } from '$lib/module/auth';
   import { userInfo } from '$lib/stores';
-  import Dashboard from 'carbon-pictograms-svelte/lib/Dashboard.svelte';
-  import UserAnalytics from 'carbon-pictograms-svelte/lib/UserAnalytics.svelte';
-  import SocialWork_01 from 'carbon-pictograms-svelte/lib/SocialWork_01.svelte';
-  import CalendarEvent from 'carbon-pictograms-svelte/lib/CalendarEvent.svelte';
-  import FileBackup from 'carbon-pictograms-svelte/lib/FileBackup.svelte';
 
   export let modules: Record<string, ModuleDefinition>;
 
