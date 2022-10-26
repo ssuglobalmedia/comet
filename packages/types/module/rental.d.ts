a6b28045bc24f38a923ff0d6f207dae5eb37ee29
Hyomin Koo <me@eatsteak.dev>
2022-10-26 11:25:38 +0900

    feat: split type definition and create Request and Response Types

---
+++ packages/types/module/rental.d.ts
@@ -0,0 +1,103 @@
+/* Rental Module */
+import type {
+  BadRequestError,
+  CometResponse,
+  ForbiddenError,
+  InternalError,
+  ModuleDao,
+  UnauthorizedError,
+  User,
+} from '../index';
+
+/* Data Objects */
+
+export type RentStatus = {
+  userId: string;
+  userName: string;
+  until: Date;
+  additionalInfo?: string;
+  announced?: boolean;
+};
+
+export type RentStatusDao = {
+  uI: { S: string };
+  uN: { S: string };
+  u: { S: string };
+  aI?: { S: string };
+  a?: { BOOL: boolean };
+};
+
+export type Goods = {
+  id: string;
+  name: string;
+  category: string;
+  permission: string;
+  location: string;
+  rentStatus?: RentStatus;
+};
+
+export type GoodsDao = {
+  n: { S: string };
+  c: { S: string };
+  p: { N: string };
+  l: { S: string };
+  rS?: { M: RentStatusDao };
+} & ModuleDao;
+
+/* Requests */
+
+export type GoodsUpdateRequest = {
+  id: string;
+  name?: string;
+  category?: string;
+  location?: string;
+  permission?: string;
+};
+
+export type GoodsDeleteRequest = {
+  id: string;
+};
+
+export type GoodsRentRequest = {
+  user: User;
+  goodsId: string;
+  until: string;
+  additionalInfo?: string;
+};
+
+export type GoodsReturnRequest = {
+  userId: string;
+  goodsId: string;
+};
+
+/* Responses */
+
+export type GoodsAddResponse = CometResponse<
+  void,
+  InternalError | BadRequestError | UnauthorizedError | ForbiddenError
+>;
+
+export type GoodsDeleteResponse = CometResponse<
+  string,
+  InternalError | BadRequestError | UnauthorizedError | ForbiddenError
+>;
+
+export type GoodsQueryResponse = CometResponse<
+  Goods[],
+  IntenralError | UnauthorizedError | ForbiddenError
+>;
+
+export type GoodsRentResponse = CometResponse<
+  boolean,
+  InternalError | BadRequestError | UnauthorizedError | ForbiddenError
+>;
+
+export type GoodsReturnResponse = CometResponse<
+  boolean,
+  InternalError | BadRequestError | UnauthorizedError | ForbiddenError
+>;
+
+export type GoodsUpdateResponse = CometResponse<
+  boolean,
+  InternalError | BadRequestError | UnauthorizedError | ForbiddenError
+>;
